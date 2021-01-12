0c363298ffa80ded7c774277871c2976743e1e1b
Henrique-temponi <tanzenheavely@gmail.com>
2021-01-12 20:04:38 -0300

    Floating precision done

---
+++ JavaChallenges/FloatingPrecision.java
@@ -0,0 +1,15 @@
+
+/**
+ * Convert a given number of pounds to kilograms
+ */
+public class FloatingPrecision {
+    public static void main(String[] args) {
+        
+        double pounds = 195.5;
+        double conversion = 0.45359237;
+        double kilograms = pounds *conversion;
+
+        System.out.println("The weight in kilograms: " + kilograms);
+        
+    }
+}
